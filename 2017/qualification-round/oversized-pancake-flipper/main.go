eddb82d6ff5f55af841f63ab09245074d72ef7ff
Pedro H. Pontes <20629214+pedrohpontes@users.noreply.github.com>
2018-05-03 21:48:16 -0400

    Add 2017 qualification problem A (#1)

---
+++ 2017/qualification-round/oversized-pancake-flipper/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// parsePancakeProblem parses the input line (as described in the problem) and
+// returns the list of pancakes and the size of the flipper (K)
+func parsePancakeProblem(line string) ([]pancake, int) {
+	splitSpace := strings.Split(line, " ")
+	if len(splitSpace) < 2 {
+		panic("wrong input to parse; should have received two strings per line")
+	}
+	pancakesStr := splitSpace[0]
+	flipperStr := splitSpace[1]
+
+	ps := make([]pancake, len(pancakesStr))
+	for i, pancakeStr := range strings.Split(pancakesStr, "") {
+		ps[i] = pancake(pancakeStr)
+	}
+
+	flipperSize, err := strconv.Atoi(flipperStr)
+	if err != nil {
+		panic(err)
+	}
+
+	return ps, flipperSize
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		panic("not enough arguments")
+	}
+
+	inputFile, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(os.Args[2])
+	if err != nil {
+		panic(err)
+	}
+	defer outputFile.Close()
+
+	scanner := bufio.NewScanner(inputFile)
+
+	count := -1
+	for scanner.Scan() {
+		count++
+		if count == 0 {
+			continue
+		}
+
+		pancakes, flipperSize := parsePancakeProblem(scanner.Text())
+		numberOfFlips := minPancakeFlips(pancakes, flipperSize)
+
+		if numberOfFlips >= 0 {
+			fmt.Fprintf(outputFile, "Case #%v: %v\n", count, numberOfFlips)
+		} else {
+			fmt.Fprintf(outputFile, "Case #%v: IMPOSSIBLE\n", count)
+		}
+	}
+}
